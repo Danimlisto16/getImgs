5b549d30d659028d3f7b3fb5cc91c537df8b6cde
dyyys0ft <dyanez4@outlook.com>
2024-04-21 23:35:52 -0500

    Prepare < not found messages>

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
-import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
-# Read the Excel file into a DataFrame
-df = pd.read_excel('./Items/New_Kalinka.xlsx')
+def search_text_in_page(url, text):
+    try:
+        # Fetch the webpage content
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for bad status codes
+        html_content = response.text
+        
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(html_content, 'html.parser')
+        
+        # Find all occurrences of the specified text
+        occurrences = soup.find_all(string=lambda t: text in str(t))
+        
+        # Print the found occurrences
+        if len(occurrences) == 0:
+            return False
+        else:
+            return True
+    except Exception as e:
+        print(f"Error occurred while accessing {url}: {e}")
 
-# Iterate over the DataFrame rows
-for index, row in df.iterrows():
-    # Access the 'Name' column and print its value
-    print(row['Name'], row['Product Code'])
+# Example usage:
+url = 'https://www.onlinetrade.ru/sitesearch.html?query=asdsads'
+text_to_search = "Найденные товары по запросу"
+print(search_text_in_page(url, text_to_search))  # Output: True
